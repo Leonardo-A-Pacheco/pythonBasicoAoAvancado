f7d785e5da05ccc28b38c68566326878ab199936
Leonardo-A-Pacheco <l00000111.pacheco@gmail.com>
2023-10-09 04:35:41 -0300

    vscodeok

--- AmbientConfig/00.py
+++ AmbientConfig/00.py
@@ -1,1 +1,29 @@
-print('oi lala')
+
+print('configuração do json vscode')
+print('extensões usadas')
+
+"""
+{
+    "files.autoSave": "afterDelay",
+    "editor.wordWrap": "on",
+    "explorer.compactFolders": false,
+    "git.autofetch": true,
+    "code-runner.runInTerminal": true,
+    "code-runner.clearPreviousOutput": true,
+    "code-runner.executorMap": {
+        "python": "cls ; python -u",
+    },
+    "code-runner.ignoreSelection": true,
+    "workbench.colorTheme": "OM Theme (Default Dracula Italic)",
+    "workbench.iconTheme": "material-icon-theme",
+    "python.defaultInterpreterPath": "python"
+
+}
+
+code runner 
+material icon theme
+Om theme
+pylance
+Python
+
+"""
