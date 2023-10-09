9caa2f97b185f4f966b08bcd7be98c94261e4749
Leonardo-A-Pacheco <l00000111.pacheco@gmail.com>
2023-10-09 05:42:25 -0300

    printFunction

---
+++ BasicLogic/a-01-ComentsDocString.py
@@ -0,0 +1,23 @@
+print("exemplo de comentario")
+#comentario
+
+print("exemplo de DocString")
+'''
+docstring ou -> """  """
+'''
+print(123)
+
+print(456)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ a-02-printFunction.py
@@ -0,0 +1,23 @@
+print("argumento do print")
+
+print("string")
+
+print(123)
+
+print(False)
+
+
+print(12,34)
+print(56,78)
+
+ # \r\n quebra de linha = CRLF
+
+print(99, 88, sep=" - ", end="\r\n")
+print(99, 88, sep=" - ", end=" ## ")
+print(99, 88, sep=" - ", end="\n\n ## ")
+print(12, 34, sep=" / ")
+print(56, 78, sep=" \ ")
+
+# separators and endline
+#pyton is case sensitive and play with identations
+# im stop 19 type str string intro data types
