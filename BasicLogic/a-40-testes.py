38edeeb75e4b942fc6125771630492821cc6322a
Leonardo-A-Pacheco <l00000111.pacheco@gmail.com>
2023-10-23 18:29:27 -0300

    72

--- BasicLogic/a-40-testes.py
+++ BasicLogic/a-40-testes.py
@@ -1,5 +1,23 @@
-start = 0
-end = 10
-while start < end:
-    print(start)
-    start += 1
+# start = 0
+# end = 10
+# while start < end:
+#     print(start)
+#     start += 1
+
+
+# start = 0
+# end = 10
+# while start < end:
+#     start += 1
+#     print(start)
+
+l = c = 4
+
+ln = 1
+while ln <= l:
+    cl = 1
+    while cl <= c:
+        print(f'linha {ln} coluna {cl}')
+        cl += 1
+    ln += 1
+    
