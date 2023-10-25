dca170db8308e9ab254047f9c03918318a385075
Leonardo-A-Pacheco <l00000111.pacheco@gmail.com>
2023-10-25 01:28:12 -0300

    94

---
+++ BasicLogic/a-51-listsInlists.py
@@ -0,0 +1,34 @@
+salas = [
+
+    ['joão', 'zé'],
+    ['maria','rebeca'],
+    ['maguila', 'molly']
+
+    ]
+
+# print(salas)
+
+# #primeira sala segundo nome
+# print('\n',salas[0][1])
+
+# print('\n----------------\n')
+
+# #terceira sala primeiro nome
+# print('\n',salas[2][0])
+
+# print('\n----------------\n')
+
+for sala in salas:
+    print(f'a sala é {sala}')
+    for aluno in sala:
+        print(f'{aluno}')
+
+
+
+
+
+
+
+
+
+
