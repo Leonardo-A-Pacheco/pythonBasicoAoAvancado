2bb0dac2691afe0c51e50a7bd628a161c1fcbbc7
Leonardo-A-Pacheco <l00000111.pacheco@gmail.com>
2023-11-23 11:39:01 -0300

    Demonstração usos dos metodos self como instância

---
+++ 05-ObjectOrientation/aula4.py
@@ -0,0 +1,25 @@
+#formas de chamada da classe
+
+class Carro:
+    
+    def __init__(self,nome='sem nome' ):
+        self.nome = nome
+    
+    def acelerar(self):
+        print(f'{self.nome} está acelerando...\n')
+
+fusca = Carro('fusca')
+fusca.acelerar()
+
+# ou
+
+Carro('celta').acelerar()
+
+# ou
+
+caminhonete = Carro('pampa')
+Carro.acelerar(caminhonete)
+
+# ou
+
+Carro().acelerar()
