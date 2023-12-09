b6941390c7798a61b9c6fda28469aa77d61153e6
Leonardo-A-Pacheco <l00000111.pacheco@gmail.com>
2023-12-08 23:11:02 -0300

    215

---
+++ 05-ObjectOrientation/aula15.py
@@ -0,0 +1,9 @@
+# Relações entre classes: associação, agregação e composição
+# Associação é um tipo de relação onde os objetos
+# estão ligados dentro do sistema.
+# Essa é a relação mais comum entre objetos e tem subconjuntos
+# como agregação e composição (que veremos depois).
+# Geralmente, temos uma associação quando um objeto tem
+# um atributo que referencia outro objeto.
+# A associação não especifica como um objeto controla
+# o ciclo de vida de outro objeto.
