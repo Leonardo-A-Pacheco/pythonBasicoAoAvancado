b6ffdbf87a233d5ae326d50c9eb34d45eaa3a934
Leonardo-A-Pacheco <l00000111.pacheco@gmail.com>
2023-12-13 22:55:55 -0300

    z

---
+++ 05-ObjectOrientation/aula18a.py
@@ -0,0 +1,25 @@
+# exercicio com classes
+# crie a classe carro none
+# crie a classe motor none
+# crie a classe fabricante none
+# faça a ligação entre carro e motor 
+# obs um motor pode ser de varios carros
+# faça a ligação entre o carro e 
+# fabricante
+# obs um fabricante pode fabricar varios carros
+# exiba o nome do carro motor e fabricante na tela
+
+
+class Carro:
+    def __init__(self) -> None:
+        ...
+
+
+
+
+
+
+
+
+
+
