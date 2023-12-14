3e49d011ad1f2529a883032d2b517f0003c88d1d
Leonardo-A-Pacheco <l00000111.pacheco@gmail.com>
2023-12-14 19:00:51 -0300

    trampando

--- 05-ObjectOrientation/aula18a.py
+++ 05-ObjectOrientation/aula18a.py
@@ -11,15 +11,57 @@
 
 
 class Carro:
-    def __init__(self) -> None:
-        ...
+    def __init__(self, nome) -> None:
+        self.nome = nome
+        self._motor= None
+        self._fabricante = None
+        
+    
+    @property
+    def motor(self):
+        return self._motor
+   
+    @motor.setter
+    def motor(self, valor):
+        self._motor = valor
+
+    
+    @property
+    def motor(self):
+        return self._motor
+   
+    @motor.setter
+    def motor(self, valor):
+        self._motor = valor
+
+    @property
+    def fabricante(self):
+        return self.fabricante
+    
+    @fabricante.setter
+    def fabricante(self, valor):
+        self._fabricante = valor
+
+class fabricante:
+    def __init__(self, nome):
+        self.nome = nome
+
+fuque = Carro('fuscao')
+fuque.motor = 'v8'
+fuque.fabricante = 'banana'
+
+print(fuque)
+
+# cria-se uma instancia para cada classe interna,
+
+fusca = Carro('Fusca')
+volkswagen = Fabricante('Volkswagen')
+motor_1_0 = Motor('1.0')
+fusca.fabricante = volkswagen
+fusca.motor = motor_1_0
+print(fusca.nome, fusca.fabricante.nome, fusca.motor.nome)
 
 
 
 
-
-
-
-
-
-
+b
