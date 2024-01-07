85b5391a6580bbedfb56057f269fc80cc7290e74
Leonardo-A-Pacheco <l00000111.pacheco@gmail.com>
2024-01-07 13:28:28 -0300

    criando log.txt 227

--- 05-ObjectOrientation/aula141/log.py
+++ 05-ObjectOrientation/aula141/log.py
@@ -1,20 +1,43 @@
 # abstração
 
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
 class Log:
-    def log(self, msg):         #assinatura do metodo
+    def _log(self, msg):         #assinatura do metodo
         raise NotImplementedError(
             'implemente o metodo log'
         )
+   
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+
+    def log_sucess(self, msg):
+        return self._log(f'Sucess: {msg}')
     
 class LogFileMixin(Log):
-      def log(self, msg):
+      def _log(self, msg):
+            msg_formatada = f'{msg} ({self.__class__.__name__})'
+            print('Selvando Log:', msg_formatada)
+            with open(LOG_FILE, 'a') as arquivo:
+                 arquivo.write(msg_formatada)
+                 arquivo.write('\n')
             print(msg)
-            
+
+class LogPrintMixin(Log):    
+      def _log(self, msg):
+           print(f'{msg} ({self.__class__.__name__}) ')
+
+
 
 if __name__ == '__main__':
-    # l = Log()
-    l = LogFileMixin()
-    l.log('coisa') 
-
+    lp = LogPrintMixin()
+    lp.log_error('DEU MERDA') 
+    lp.log_sucess('DEU OURO') 
+    lf = LogFileMixin()
+    lf.log_error('DEU MERDA') 
+    lf.log_sucess('DEU OURO') 
+    print(LOG_FILE)
 
 
--- 05-ObjectOrientation/aula141/main.py
+++ 05-ObjectOrientation/aula141/main.py
@@ -1,8 +1,14 @@
 # abstração
 # print('oi')
 
-from log import Log
+from log import LogPrintMixin, LogFileMixin
 
+lp = LogPrintMixin()
+lp.log_error('DEU MERDA') 
+lp.log_sucess('DEU OURO') 
+lf = LogFileMixin()
+lf.log_error('DEU MERDA') 
+lf.log_sucess('DEU OURO') 
 
 
 
