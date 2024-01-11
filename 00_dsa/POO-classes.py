c54daf224e2b4cf03bbb7d9021d22ac4f1f7cf9a
Leonardo-A-Pacheco <l00000111.pacheco@gmail.com>
2024-01-10 22:08:45 -0300

    criando classe com python

---
+++ 00_dsa/POO-classes.py
@@ -0,0 +1,21 @@
+class Livro():
+    def __init__(self):
+        self.titulo = 'titulo do livro'
+        self.isbn = 123134345
+        # print('construtor chamado para criar o objeto dessa classe')
+
+    def Imprime(self):
+        print(f'livro: {self.titulo} ISBN:{self.isbn}')
+
+
+l = Livro()
+
+print(type(l))
+print(l.titulo)
+print(l.isbn)
+
+print(l.Imprime())
+
+l.Imprime()
+
+
