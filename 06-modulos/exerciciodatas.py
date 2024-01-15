d5dc215db07b602705c82c474548a808233c821b
Leonardo-A-Pacheco <l00000111.pacheco@gmail.com>
2024-01-15 19:57:24 -0300

    calculo com datas

--- 06-modulos/exerciciodatas.py
+++ 06-modulos/exerciciodatas.py
@@ -7,3 +7,30 @@
 # - Crie a data do empréstimo
 # - Crie a data do final do empréstimo
 # - Mostre todas as datas de vencimento e o valor de cada parcela
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+valor_total = 1_000_000
+data_emprestimo = datetime(20, 12, 20)
+delta_anos = relativedelta(years=5)
+data_final = data_emprestimo + delta_anos
+
+data_parcelas = []
+data_parcela = data_emprestimo
+while data_parcela < data_final:
+    data_parcelas.append(data_parcela)
+    data_parcela += relativedelta(months=+1)
+
+n_parcelas = len(data_parcelas)
+valor_parcela = valor_total / n_parcelas
+
+for data in data_parcelas:
+    print(data.strftime('%d/%m/%Y'), f'R$ {valor_parcela:,.2f}')
+
+print()
+
+print(
+    f'Você pegou {valor_total:,.2f} emprestado\n'
+    f'Em {delta_anos.years} anos, ({n_parcelas}) meses) \n'
+    f'em parcelas de R$:{valor_parcela:,.2f}.\n\n'
+)
